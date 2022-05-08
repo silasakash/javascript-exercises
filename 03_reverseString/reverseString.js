cf34bcd81aecee16242ca08e57326329825f79dd
akashsilas <akashsilas1@gmail.com>
2022-05-08 18:00:43 +0530

    Modify reverseString.js reverseString.spec.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string1) {
+    let reversedString = '';
+    for (let i = string1.length-1;i >= 0;i--) {
+        reversedString += string1[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
