18bc3fec8f8e677e009c28fab8b6d7bb1a68b499
akashsilas <akashsilas1@gmail.com>
2022-05-08 17:45:21 +0530

    Modify repeatString.jsand repeatString.spec.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let opString = '';
+    for (let i = 1;i <= num; i++) {
+        opString += string;
+    }
+    if (num < 0) return "ERROR";
+    return opString;
 };
 
 // Do not edit below this line
