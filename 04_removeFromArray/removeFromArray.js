b74ba98a361408fced7abe539123b41ef2622323
akashsilas <akashsilas1@gmail.com>
2022-05-08 21:22:17 +0530

    Change removeFromArray.js and implement soft-coded function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,20 +1,10 @@
-const removeFromArray = function(userArray, userArgument1=null,userArgument2=null,userArgument3=null,userArgument4=null) {
+const removeFromArray = function(userArray, ...args) {
     let arrayOp = userArray;
-    if (userArray.includes(userArgument1)) {
-        const index1 = userArray.indexOf(userArgument1);
-        userArray.splice(index1,1);
-    }
-    if (userArray.includes(userArgument2)) {
-        const index2 = userArray.indexOf(userArgument2);
-        userArray.splice(index2,1);
-    }
-    if (userArray.includes(userArgument3)) {
-        const index3 = userArray.indexOf(userArgument3);
-        userArray.splice(index3,1);
-    }
-    if (userArray.includes(userArgument4)) {
-        const index4 = userArray.indexOf(userArgument4);
-        userArray.splice(index4,1);
+    for (let arg of args) {
+        if (arrayOp.includes(arg)) {
+            let index = arrayOp.indexOf(arg);
+            arrayOp.splice(index,1);
+        }
     }
     return arrayOp;
 };
