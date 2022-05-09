d45af2181964e199521d3ada3d92c3a03a7c65ab
akashsilas <akashsilas1@gmail.com>
2022-05-09 21:25:04 +0530

    Modify tempConversion.js to pass the tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahr) {
+  let cels = (fahr-32)*5/9;
+  let roundedcels = Math.round(cels * 10) / 10;
+  return roundedcels;
 };
 
-const ctof = function() {
-
+const ctof = function(cels) {
+  let fahr = cels * 9 / 5 + 32;
+  let roundedfahr = Math.round(fahr * 10) / 10;
+  return roundedfahr;
 };
 
 // Do not edit below this line
