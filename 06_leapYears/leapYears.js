3b5a3a760e8dd2b976773bee03803aa96c3270b4
akashsilas <akashsilas1@gmail.com>
2022-05-09 21:13:00 +0530

    Modify leapYears.js to pass the tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(leapYear) {
+    if (leapYear % 4 == 0) {
+        if (leapYear % 100 == 0) {
+            if (leapYear % 400 == 0) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    else return false;
 };
 
 // Do not edit below this line
